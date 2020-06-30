43d6a6226cfda7563827eed753eb89e3b76978b6
jion-kozono <zeon66190329@gmail.com>
2020-06-30 17:33:10 +0900

    changed Header and NavBar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,41 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { AppBar, Toolbar, Button } from '@material-ui/core'
-import AccountCircleIcon from '@material-ui/icons/AccountCircle'
+import { AppBar, Toolbar } from '@material-ui/core'
 import NavBar from '../NavBar'
 
 const Header = () => {
-    const LinkStyle = {
-        textDecoration: "none",
-        color: "#0000ff"
-    }
     return (
         <>
             <AppBar position="static" color="default">
                 <Toolbar>
                     <h2>Translater</h2>
-                    <Link to="/signUp" style={LinkStyle}>
-                        <Button variant="contained" color="default">
-                            SignUp
-                        </Button>
-                    </Link>
-                    <Link to="/logIn" style={LinkStyle}>
-                        <Button variant="contained" color="default">
-                            LogIn
-                        </Button>
-                    </Link>
-                    <Link to="/" style={LinkStyle}>
-                        <Button variant="contained" color="default">
-                            LogOut
-                        </Button>
-                    </Link>
                     <NavBar />
-                    <Link to="/userShow" style={LinkStyle}>
-                        <Button variant="outlined" color="default">
-                            <AccountCircleIcon />
-                            Profile
-                        </Button>
-                    </Link>
                 </Toolbar>
             </AppBar>
         </>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,37 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle'
+import { Button } from '@material-ui/core'
 
-function NavBar() {
+const NavBar = () => {
+    const LinkStyle = {
+        textDecoration: "none",
+        color: "#0000ff"
+    }
     return (
         <>
-            <Link to="/">英文一覧</Link><br/>
+            <Link to="/" style={LinkStyle}>英文一覧</Link><br />
+            <Link to="/signUp" style={LinkStyle}>
+                <Button variant="contained" color="default">
+                    SignUp
+                </Button>
+            </Link>
+            <Link to="/logIn" style={LinkStyle}>
+                <Button variant="contained" color="default">
+                    LogIn
+                </Button>
+            </Link>
+            <Link to="/" style={LinkStyle}>
+                <Button variant="contained" color="default">
+                    LogOut
+                </Button>
+            </Link>
+            <Link to="/userShow" style={LinkStyle}>
+                <Button variant="outlined" color="default">
+                    <AccountCircleIcon />
+                    Profile
+                </Button>
+            </Link>
         </>
     )
 }
